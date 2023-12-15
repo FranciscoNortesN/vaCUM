fe12390d72ed53ccd7529260818c18add45b7f0f
FranciscoNortesN <147821042+FranciscoNortesN@users.noreply.github.com>
2023-12-15 21:32:52 +0100

    hay muchas cosas que van mal

--- vaCUM (experimentación personal).c
+++ vaCUM (experimentación personal).c
@@ -14,7 +14,7 @@
 #define p -1 //pared
 
 
-enum direcciones {
+typedef enum {
 	arriba,
 	esquina_arriba_derecha,
 	derecha,
@@ -23,7 +23,7 @@ enum direcciones {
 	esquina_abajo_izquierda,
 	izquierda,
 	esquina_arriba_izquierda
-};
+}direcciones;
 /*si la posicion del robot es x, con lo que viene ahora representamos las posiciones relativas a x
 701
 6x2
@@ -69,6 +69,57 @@ void limpiar_basura (int mapa_principal[filas][columnas],Posicion pos/*,ll migui
 	}
 }
 
+int visitado (int miguitas_de_pan[filas][columnas],Posicion pos){
+	int tx[8]={0,1,1,1,0,-1,-1,-1},ty[8]={1,1,0,-1,-1,-1,0,1};
+	//comprobamos las casillas de alrededor a las que está el robot en miguitas de pan en busca de una que no haya visitado
+	for(int i=0;i<8;i++){
+		if(miguitas_de_pan[pos.x+tx[i]][pos.y+ty[i]]==0){
+			return i;
+		}
+	}
+	return -1;
+}
+
+void desatasco (int mapa_principal[filas][columnas],Posicion *pos,int miguitas_de_pan[filas][columnas],ll *atascado,direcciones *direccion,ll *casillas_por_recorrer,ll *casillas_recorridas){
+	*direccion=(*direccion+4)%8;
+	while(miguitas_de_pan[pos->x][pos->y]!=0&&casillas_recorridas<casillas_por_recorrer){
+		int tx[8]={0,1,1,1,0,-1,-1,-1},ty[8]={1,1,0,-1,-1,-1,0,1};
+		int dx=tx[*direccion];
+		int dy=ty[*direccion];
+		if(visitado(miguitas_de_pan,*pos)!=-1){
+			*atascado=0;
+			dx=tx[visitado(miguitas_de_pan,*pos)];
+			dy=ty[visitado(miguitas_de_pan,*pos)];
+			pos->x+=dx;
+			pos->y+=dy;
+			(*casillas_recorridas)++;
+			printf("x%d, y%d, %lld\n", pos->x, pos->y, (long long int)mapa_principal[pos->x][pos->y]);
+			miguitas_de_pan[pos->x][pos->y]=1;
+			limpiar_basura(mapa_principal,*pos);
+			break;
+		}
+		else{
+			if (mapa_principal[pos->x+dx][pos->y+dy]!=p&&miguitas_de_pan[pos->x+dx][pos->y+dy]==1/*este 1 va a dar problemas en el futuro*/){
+				miguitas_de_pan[pos->x+dx][pos->y+dy]++;
+				pos->x+=dx;
+				pos->y+=dy;
+				*atascado=0;
+				printf("x%d, y%d, %lld\n", pos->x, pos->y, (long long int)mapa_principal[pos->x][pos->y]);
+				limpiar_basura(mapa_principal,*pos);
+			}
+			else{
+				*direccion=(*direccion+2)%8;
+				if(mapa_principal[pos->x+dx][pos->y+dy]==p){//si hay pared
+					if(miguitas_de_pan[pos->x+dx][pos->y+dy]!=p){
+						*casillas_por_recorrer--;
+						printf("x%d, y%d, %lld\n", pos->x, pos->y, (long long int)mapa_principal[pos->x][pos->y]);
+					}
+					miguitas_de_pan[pos->x+dx][pos->y+dy]=p;//nos guardamos que hay una pared
+				}
+			}
+		}
+	}	
+}
 
 int main(int argc, char *argv[])
 {
@@ -77,7 +128,7 @@ int main(int argc, char *argv[])
 	int mapa_principal [filas][columnas]={{p,p,p,p,p,p},
 	{p,0,0,0,0,p},
 	{p,0,0,3,0,p},
-	{p,0,0,0,0,p},
+	{p,0,p,0,0,p},
 	{p,0,p,0,0,p},
 	{p,p,p,p,p,p}};
 	//mapa que te dan
@@ -100,19 +151,21 @@ int main(int argc, char *argv[])
 	Posicion pos={1,1};//coordenadas del robot
 	ll bateria=bateria_max;//bateria del robot
 	ll hx=pos.x,hy=pos.y;//nos guardamos la base de carga
-	enum direcciones direccion=0;//0 arriba, 1 esquina_arriba_derecha, 2 derecha, 3 esquina_abajo_derecha, 4 abajo, 5 esquina_abajo_izquierda, 6 izquierda, 7 esquina_arriba_izquierda
+	direcciones direccion=0;//0 arriba, 1 esquina_arriba_derecha, 2 derecha, 3 esquina_abajo_derecha, 4 abajo, 5 esquina_abajo_izquierda, 6 izquierda, 7 esquina_arriba_izquierda
 	ll atascado=0;//si llega a 8 es que esta atascado
 	miguitas_de_pan[pos.x][pos.y]=1;//por aqui ya ha pasado
-	ll casillas_recorridas=0;//casillas recorridas
+	ll casillas_recorridas=1;//casillas recorridas
 	ll casillas_por_recorrer=(filas-2)*(columnas-2);//casillas por recorrer
 
-	while(casillas_recorridas<=casillas_por_recorrer){
+	printf_pos_map;
+
+	while(casillas_recorridas<casillas_por_recorrer){
 		int tx[8]={0,1,1,1,0,-1,-1,-1},ty[8]={1,1,0,-1,-1,-1,0,1};
 		int dx=tx[direccion];
 		int dy=ty[direccion];
 		if(atascado<=8){
-			if (mapa_principal[pos.x+dx][pos.y+dy]!=p && miguitas_de_pan[pos.x+dx][pos.y+dy]!=1){//si no hay pared, si tiene bateria y si no ha pasado por ahi
-				miguitas_de_pan[pos.x+dx][pos.y+dy]=1;//por aqui ya ha pasado
+			if (mapa_principal[pos.x+dx][pos.y+dy]!=p && miguitas_de_pan[pos.x+dx][pos.y+dy]==0){//si no hay pared, si tiene bateria y si no ha pasado por ahi
+				miguitas_de_pan[pos.x+dx][pos.y+dy]++;//por aqui ya ha pasado
 				pos.x+=dx;//se mueve
 				pos.y+=dy;
 				bateria--;//gasta bateria
@@ -123,8 +176,10 @@ int main(int argc, char *argv[])
 			}
 			else{
 				if(mapa_principal[pos.x+dx][pos.y+dy]==p){//si hay pared
-					if(miguitas_de_pan[pos.x+dx][pos.y+dy]!=p)
+					if(miguitas_de_pan[pos.x+dx][pos.y+dy]!=p){
 						casillas_por_recorrer--;
+						printf_pos_map;
+					}
 					miguitas_de_pan[pos.x+dx][pos.y+dy]=p;//nos guardamos que hay una pared
 				}
 				giro90;//gira 90 grados a la derecha
@@ -133,6 +188,8 @@ int main(int argc, char *argv[])
 		}
 		else{
 			//código para mover a la aspiradora a un lugar donde no esté atascada
+			desatasco(mapa_principal,&pos,miguitas_de_pan,&atascado,&direccion,&casillas_por_recorrer,&casillas_recorridas);
+			casillas_recorridas++;
 		}
 	}
 

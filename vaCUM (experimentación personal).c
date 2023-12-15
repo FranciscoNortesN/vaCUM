a756bf88ebf36d4da8dfe81dfff2a3aa0a64f3bc
FranciscoNortesN <147821042+FranciscoNortesN@users.noreply.github.com>
2023-12-15 15:08:04 +0100

    Update vaCUM (experimentación personal).c

--- vaCUM (experimentación personal).c
+++ vaCUM (experimentación personal).c
@@ -35,7 +35,8 @@ enum direcciones {
 //esta macro está, porque la vamos a usar mucho y es mas facil de escribir
 #define printf_pos_pan printf("x%d, y%d, %lld\n", pos.x, pos.y, (long long int)miguitas_de_pan[pos.x][pos.y])//imprime la posicion del robot y el estado de la casilla de las miguitas de pan
 //lo mismo que la anterior macro pero con las miguitas de pan
-
+#define giro45 direccion=(direccion+1)%8//gira a la derecha 45 grados
+#define giro90 giro45;giro45//gira a la derecha 90 grados
 
 typedef long long ll;
 typedef float f;
@@ -46,17 +47,6 @@ typedef struct {
     int x;
     int y;
 } Posicion;
-
-void giro (enum direcciones *direccion){//gira a la derecha 45 grados y suma 1 al contador de atascado (si llega a 8 es que esta atascado)
-	if (*direccion==arriba) *direccion=esquina_arriba_derecha;
-	else if (*direccion==esquina_arriba_derecha) *direccion=derecha;
-	else if (*direccion==derecha) *direccion=esquina_abajo_derecha;
-	else if (*direccion==esquina_abajo_derecha) *direccion=abajo;
-	else if (*direccion==abajo) *direccion=esquina_abajo_izquierda;
-	else if (*direccion==esquina_abajo_izquierda) *direccion=izquierda;
-	else if (*direccion==izquierda) *direccion=esquina_arriba_izquierda;
-	else if (*direccion==esquina_arriba_izquierda) *direccion=arriba;
-}
 
 void imprimirTablero(int tablero[filas][columnas]) {//imprime el tablero
     for (int i = 0; i < filas; i++) {
@@ -82,6 +72,8 @@ void limpiar_basura (int mapa_principal[filas][columnas],Posicion pos/*,ll migui
 
 int main(int argc, char *argv[])
 {
+	srand(time(NULL));
+	
 	int mapa_principal [filas][columnas]={{p,p,p,p,p,p},
 	{p,0,0,0,0,p},
 	{p,0,0,3,0,p},
@@ -118,30 +110,34 @@ int main(int argc, char *argv[])
 		int tx[8]={0,1,1,1,0,-1,-1,-1},ty[8]={1,1,0,-1,-1,-1,0,1};
 		int dx=tx[direccion];
 		int dy=ty[direccion];
-		if (mapa_principal[pos.x+dx][pos.y+dy]!=p && miguitas_de_pan[pos.x+dx][pos.y+dy]!=1){//si no hay pared, si tiene bateria y si no ha pasado por ahi
-			miguitas_de_pan[pos.x+dx][pos.y+dy]=1;//por aqui ya ha pasado
-			pos.x+=dx;//se mueve
-			pos.y+=dy;
-			bateria--;//gasta bateria
-			atascado=0;//resetea el contador de atascado
-			printf_pos_map;
-			casillas_recorridas++;
-			limpiar_basura(mapa_principal,pos);
+		if(atascado<=8){
+			if (mapa_principal[pos.x+dx][pos.y+dy]!=p && miguitas_de_pan[pos.x+dx][pos.y+dy]!=1){//si no hay pared, si tiene bateria y si no ha pasado por ahi
+				miguitas_de_pan[pos.x+dx][pos.y+dy]=1;//por aqui ya ha pasado
+				pos.x+=dx;//se mueve
+				pos.y+=dy;
+				bateria--;//gasta bateria
+				atascado=0;//resetea el contador de atascado
+				printf_pos_map;
+				casillas_recorridas++;
+				limpiar_basura(mapa_principal,pos);
+			}
+			else{
+				if(mapa_principal[pos.x+dx][pos.y+dy]==p){//si hay pared
+					if(miguitas_de_pan[pos.x+dx][pos.y+dy]!=p)
+						casillas_por_recorrer--;
+					miguitas_de_pan[pos.x+dx][pos.y+dy]=p;//nos guardamos que hay una pared
+				}
+				giro90;//gira 90 grados a la derecha
+				atascado++;//suma 1 al contador de atascado
+			}
 		}
 		else{
-			if(mapa_principal[pos.x+dx][pos.y+dy]==p){//si hay pared
-				if(miguitas_de_pan[pos.x+dx][pos.y+dy]!=p)
-					casillas_por_recorrer--;
-				miguitas_de_pan[pos.x+dx][pos.y+dy]=p;//nos guardamos que hay una pared
-			}
-			giro(&direccion);//gira a la derecha
-			atascado++;//suma 1 al contador de atascado
-			giro(&direccion);//gira a la derecha
+			//código para mover a la aspiradora a un lugar donde no esté atascada
 		}
 	}
 
-	imprimirTablero(mapa_principal);
-	imprimirTablero(miguitas_de_pan);
+		imprimirTablero(mapa_principal);
+		imprimirTablero(miguitas_de_pan);
 
 
 

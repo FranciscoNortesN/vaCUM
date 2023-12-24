8feb9c5d85feceb87b9fdd9178fd7daf44dc6263
FranciscoNortesN <147821042+FranciscoNortesN@users.noreply.github.com>
2023-12-24 09:50:06 +0100

    Update vaCUM (implementación final).c
    perdón por no haberlo actualizado en su momento

--- vaCUM (implementación final).c
+++ vaCUM (implementación final).c
@@ -1,3 +1,6 @@
+//implementar funciones spiderman 
+//home_coming & far_from_home
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -9,7 +12,8 @@
 #define filas 30
 #define columnas 50
 #define dimension 3
-#define BATERIA_MAX 1000
+#define bateria_max 1000
+#define sabastian gay
 
 struct coor{
   int x, y;
@@ -40,6 +44,13 @@ direcciones direccion;//direccion del robot
 ll atascado=0;//si llega a 8 es que esta atascado
 Posicion pos;
 ll miguitas_de_pan[filas][columnas][dimension]={0};
+ll miguitas=1;
+bool base=false;
+bool full=true;
+/*
+dimensión=0: mapa donde con cada paso que se realiza en una casilla nueva, se deja un número mayor
+dimensión=1: mapa donde con cada paso que se realiza en una casilla nueva, se deja un 1, con cada casilla ya visitada, se deja un 2 si se ha visitado 2 veces, un 3 si 3...
+dimensión=2: mapa que guarda el rastro que deja la función de home_coming para luego regresar*/
 int tx[8]={0,1,1,1,0,-1,-1,-1},ty[8]={1,1,0,-1,-1,-1,0,1};
 
 void giro45 (){
@@ -83,18 +94,71 @@ void limpiar_basura(){
   }
 }
 
+void girar(int d){
+  switch(d){
+    case 1:
+      giro45();
+      break;
+    case 2:
+      giro90();
+      break;
+    case 3:
+      giro135();
+      break;
+    case 4:
+      giro180();
+      break;
+    case 5:
+      giromenos135();
+      break;
+    case 6:
+      giromenos90();
+      break;
+    case 7:
+      giromenos45();
+      break;
+    default:
+      break;
+  }
+}
+
+void home_coming(){
+  int menor = 2, rastro = 0, j;
+  if (rmb_battery() < (bateria_max/2)){
+    for (int i = 0; i < 8; i++){
+      if (miguitas_de_pan[pos.x + tx[i]][pos.y + ty[i]][0] < miguitas_de_pan[pos.x + tx[menor][pos.y] + ty[menor]][0]){   
+        menor = i;
+        j=i;
+      }
+    }
+    miguitas_de_pan[pos.x + tx[j]][pos.y + ty[j]][2] = rastro++;
+  }  
+  girar(menor);
+}
+
+void far_from_home(){
+    for (int i = 0; i < 8; i++){
+      if (miguitas_de_pan[pos.x + tx[i]][pos.y + ty[i]][2] < miguitas_de_pan[pos.x + tx[menor][pos.y + ty[menor]][0]){
+        
+      }
+    }
+}
+      
 void desatasco(){
   bool primera=true;
   int menor;
-  for(int i=8;i>=0;i--){
+  int d;
+  for(int i=7;i>=0;i--){
     if(miguitas_de_pan[pos.x+tx[i]][pos.y+ty[i]][0]>=0 && miguitas_de_pan[pos.x+tx[i]][pos.y+ty[i]][0]!=p && primera==true){
       menor=miguitas_de_pan[pos.x+tx[i]][pos.y+ty[i]][0];
       primera=false;
     }
     else if(miguitas_de_pan[pos.x+tx[i]][pos.y+ty[i]][0]<menor){
       menor=miguitas_de_pan[pos.x+tx[i]][pos.y+ty[i]][0];
+      d=i;
     }
   }
+  girar(d);//se orienta hacia la casilla menor
 }
 
 void explorar(){
@@ -108,7 +172,7 @@ void explorar(){
   }  
 }
 
-void empareda(){//rellena los tres mapas con las paredes
+void empareda2(){//rellena los tres mapas con las paredes
   for(int i=0; i<filas; i++){
     for(int j=0; j<dimension; j++){
       miguitas_de_pan[i][0][j]=p;
@@ -125,36 +189,53 @@ void empareda(){//rellena los tres mapas con las paredes
   
 void on_start(){
   rmb_awake(&pos.x, &pos.y);
-  empareda();
+  empareda2();
   sensor_t rob_state = rmb_state();
-  direccion=rob_state.head/90+6;
+  direccion=(rob_state.head/90+6)%8;
   giro90();
 }
 
 
 void cyclic_behav(){
-  //da un paso en la dirección actual
-  if (rmb_bumper()==0 && miguitas_de_pan[pos.x+dx][pos.y+dy][0]==0 && rmb_battery<BATERIA_MAX/2){//si no hay pared, si tiene bateria y si no ha pasado por ahi
-    rmb_forward();//se mueve
-    limpiar_basura();
-    int dx=tx[direccion];
-    int dy=ty[direccion];
-    pos.x+=dx;
-    pos.y+=dy;
-    miguitas_de_pan[pos.x][pos.y][0]=miguitas++;//por aqui ya ha pasado
-    atascado=0;//resetea el contador de atascado
+  if(rmb_battery()>bateria_max/2&& base==false && full==true){
+    if(atascado<8){
+      int dx=tx[direccion];
+      int dy=ty[direccion];
+      if (rmb_bumper()==0 && miguitas_de_pan[pos.x+dx][pos.y+dy][0]==0){//si no hay pared, si tiene bateria y si no ha pasado por ahi
+        rmb_forward();//se mueve
+        limpiar_basura();
+        pos.x+=dx;
+        pos.y+=dy;
+        miguitas_de_pan[pos.x][pos.y][0]=miguitas++;//por aqui ya ha pasado
+        miguitas_de_pan[pos.x][pos.y][1]++;
+        atascado=0;//resetea el contador de atascado
+      }
+      else{
+        explorar();//gira hacia la primera casilla donde ha encontrado un cero en sentido horario
+      }
+    }
+    else{
+      desatasco();
+      rmb_forward();
+    }
   }
-  else if(rmb_bumper()==1 && bateria<BATERIA_MAX/2){
-    explorar();//gira hacia la primera casilla donde ha encontrado un cero en sentido horario
+  else if(rmb_at_base() && bateria<bateria_max){
+    volver_base();
+    rmb_forward();
+    if(rmb_at_base()){
+        base=true;
+    }
   }
-  //deteccion de suciedad
-  limpiar_basura();
-  //carga si se encuentra en la base
-  if(rmb_at_base())
+  else if(rmb_at_base()){
     while(rmb_battery() < 250)
       rmb_load();
+    full=true;
+  }
+  else{
+    far_from_home();
+  }
+    
 }
-
 
 void on_stop(){
   visualize();
